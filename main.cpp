067e941cb9dd90b50caf96229caf503b6d291f85
poluianov24 <poluianov.vlad.24@gmail.com>
2024-03-02 22:14:12 +0200

    Exercise is completed

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int x;
+	int * ptr_x = &x;
+
+	cout << "¬вед≥ть число: ";
+	cin >> *ptr_x;
+
+	if (x < 0)
+	{
+		cout << endl << "„исло: " << *ptr_x << " - Ї в≥д'Їмним." << endl;
+	}
+	else
+	{
+		cout << endl << "„исло: " << *ptr_x << " - Ї додатн≥м." << endl;
+	}
+
+	return 0;
+}
